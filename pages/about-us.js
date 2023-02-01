73c226b8a14af3a968a6b43fd6ec436486107915
oscarsangpa <oscarsangrador@gmail.com>
2023-02-01 17:11:10 +0100

    add basic routes

---
+++ pages/about-us.js
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+function AboutUs() {
+  return (
+    <div>
+    <h2>About Us</h2>
+
+    <Link href="/">Go Home</Link>
+    </div>
+  )
+}
+
+export default AboutUs
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
+import Link from "next/link"
 
 export default function Home() {
   return (
     <>
     <h1>First Steps Next.js!</h1>
+    <Link href="/about-us">About Us</Link>
+    <Link href="/blog">Blog</Link>
+    <Link href="/cart">Cart</Link>
     </>
   )
 }
