cf3ed8fc84e09c53398d1a747b4d156d32951d74
oscarsangpa <oscarsangrador@gmail.com>
2023-02-01 20:03:56 +0100

    styles: footer

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import Link from "next/link"
+import styles from "../styles/footer.module.css"
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className={styles.footer}>
+      <div className={`container ${styles.contain}`}>
+        <nav className={styles.navegation}>
+      <Link href="/">
+        Home
+      </Link>
+
+      <Link href="/about-us">
+        About Us
+      </Link>
+
+      <Link href="/blog">
+        Blog
+      </Link>
+
+      <Link href="/store">
+        Store
+      </Link>
+        </nav>
+          <p className={styles.copyright}> Copyrigth { new Date().getFullYear()}</p>
+      </div>
+
+    </footer>
   )
 }
 
