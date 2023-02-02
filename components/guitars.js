0c2b73af7b58909c82fb327e3b63ddc445dfd5e1
oscarsangpa <oscarsangrador@gmail.com>
2023-02-02 18:35:16 +0100

    style: store and guitar component

--- components/guitars.js
+++ components/guitars.js
@@ -1,16 +1,23 @@
 import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/guitars.module.css"
 
 const Guitars = ( { guitar } ) => {
 
   const {name, description, price, url, image } = guitar;
-  const { imageGuitar } = image.data.attributes.formats.medium.url
+  // const { imageGuitar } = image.data.attributes.formats.medium.url
 
   
   return (
-    <div>
-      <p>{name}</p>
+    <div className={styles.guitar}>
       <Image src={image.data.attributes.formats.medium.url} width={600} height={400} alt={`guitar ${name} image`}/>
 
+      <div className={styles.contain}>
+        <p>{name}</p>
+        <p className={styles.description}>{description}</p>
+        <p className={styles.price}>{price}€</p>
+        <Link className={styles.link} href={`/guitars/${url}`}>More details </Link>
+      </div>
     </div>
   )
 }
--- pages/store.js
+++ pages/store.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Guitars from '../components/guitars'
 import Layout from '../components/layout'
+import styles from "../styles/grid.module.css"
+
 
 export async function getStaticProps() {
   const response = await fetch(`${process.env.API_URL}/guitars?populate=image`)
@@ -21,12 +23,14 @@ const Store = ({guitars}) => {
         <main className='container'>
           <h2 className='heading'>Our Collection</h2>
 
-         {guitars.map(guitar => (
+        <div className={styles.grid}>
+         {guitars?.map(guitar => (
           <Guitars
             key={guitar.id}
             guitar={guitar.attributes}
           />
          ))}
+        </div>
 
         </main>
     </Layout>
