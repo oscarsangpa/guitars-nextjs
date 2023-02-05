bd903b751d6d3e06b8f5b9afae126d4a14db3be5
oscarsangpa <oscarsangrador@gmail.com>
2023-02-05 10:47:09 +0100

    feat: using nextjs context to add/delete/update products

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,55 @@
+import { useState } from 'react';
 import '../styles/globals.css';
 
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const [cart, setCart] = useState([])
+
+
+  const addToCart = guitar => {
+    // change if guitar is in the cart
+    if (cart.some(guitarState => guitarState.id === guitar.id)) {
+      // Iteration for amount update
+      const updateCart = cart.map(guitarState => {
+        if (guitarState.id === guitar.id) {
+          guitarState.amount = guitar.amount;
+        }
+        return guitarState;
+      });
+
+
+      setCart([...updateCart]);
+      localStorage.setItem('cart', JSON.stringify(cart));
+    } else {
+      setCart([...cart, guitar]);
+      localStorage.setItem('cart', JSON.stringify(cart));
+    }
+  }
+
+  const deleteProduct = id => {
+    const updateCart = cart.filter(product => product.id != id)
+    setCart(updateCart)
+    window.localStorage.setItem('cart', JSON.stringify(cart));
+  }
+
+  const updateAmount = guitar => {
+    const updateCart = carrt.map(guitarState => {
+      if (guitarState.id === guitar.id) {
+        guitarState.amount = parseInt(guitar.amount)
+      }
+      return guitarState
+    })
+    setCart(updateCart)
+    window.localStorage.setItem('cart', JSON.stringify(cart));
+  }
+
+  return <Component {...pageProps}
+    cart={cart}
+    addToCart={addToCart}
+    deleteProduct={deleteProduct}
+    updateAmount={updateAmount}
+  />
 }
 
 export default MyApp
--- pages/guitars/[url].js
+++ pages/guitars/[url].js
@@ -1,42 +1,67 @@
 import Image from "next/image";
 import styles from "../../styles/guitars.module.css"
 import Layout from "../../components/layout";
+import { useState } from "react";
 
-export default function GuitarProduct ({guitar}) {
+export default function GuitarProduct({ guitar, addToCart }) {
 
-  const {name, description, image, price} = guitar[0].attributes
+  const [amount, setAmount] = useState(0)
+  const { name, description, image, price } = guitar[0].attributes
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (amount < 1) {
+      alert("Amount not valid")
+      return
+    }
+
+    const selectGuitar = {
+      id: guitar[0].id,
+      image: image.data.attributes.url,
+      name,
+      price,
+      amount,
+    }
+
+    addToCart(selectGuitar)
+
+  }
 
   return (
     <Layout
       title={`${name}'s guitar`}
     >
-    <div className={styles.guitar}>
-    <Image src={image.data.attributes.url} width={600} height={400} alt={`guitar ${name} image`}/>
+      <div className={styles.guitar}>
+        <Image src={image.data.attributes.url} width={600} height={400} alt={`guitar ${name} image`} />
 
-    <div className={styles.contain}>
-      <p>{name}</p>
-      <p className={styles.description}>{description}</p>
-      <p className={styles.price}>{price}€</p>
+        <div className={styles.contain}>
+          <p>{name}</p>
+          <p className={styles.description}>{description}</p>
+          <p className={styles.price}>{price}€</p>
 
-    <form className={styles.form}>
-        <label htmlFor="amount">Amount: </label>
-      <select id="amount">
-        <option value="0">-- Select --</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-      </select>
-      <input 
-        type="submit"
-        value="Add to cart"      
-      />
-    </form>
-    </div>
+          <form
+            onSubmit={handleSubmit} 
+            className={styles.form}>
+            <label htmlFor="amount">Amount: </label>
+            <select
+              onChange={e => setAmount(+e.target.value)}
+              id="amount">
+              <option value="0">-- Select --</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+            <input
+              type="submit"
+              value="Add to cart"
+            />
+          </form>
+        </div>
 
-  </div>
+      </div>
     </Layout>
   )
 }
@@ -61,15 +86,15 @@ export async function getStaticPaths() {
 
 }
 
-export async function getStaticProps( { params: {url} } ){
+export async function getStaticProps({ params: { url } }) {
   const response = await fetch(`${process.env.API_URL}/guitars?filters[url]=${url}&populate=image`)
-  const {data: guitar } = await response.json()
+  const { data: guitar } = await response.json()
 
 
   return {
-      props: {
-         guitar
-      }
+    props: {
+      guitar
+    }
   }
 }
 
